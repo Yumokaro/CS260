c750bdf37cc30806c8db34454955d1b3d1326c84
Yumokaro <yumokaro18@gmail.com>
2020-08-17 13:00:49 -0700

    create Assignment 04

---
+++ Assignment04/Main.h
@@ -0,0 +1,18 @@
+#ifndef CS260_MAIN_H
+#define CS260_MAIN_H
+
+//#include <array>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using namespace std;
+
+int main () {
+    cout << "hello" << endl;
+
+    return 0;
+};
+
+
+#endif //CS260_MAIN_H
