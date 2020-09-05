0dc0e180b4e29634950fc9cc7ccdb912b37923d7
Yumokaro <yumokaro18@gmail.com>
2020-09-04 22:01:01 -0700

    add to insert, >= move to right

--- Assignment06/Main.cpp
+++ Assignment06/Main.cpp
@@ -28,7 +28,7 @@ Node *insertNode(Node *root, int value) {
         return newNode(value);
     } else if (value < root->value) {
         root->left = insertNode(root->left, value);
-    } else if (value > root->value) {
+    } else if (value => root->value) {
         root->right = insertNode(root->right, value);
     }
 
