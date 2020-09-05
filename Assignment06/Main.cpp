8002b56c21999b1fcc8a8908b3656fb579e0039d
Yumokaro <yumokaro18@gmail.com>
2020-09-04 21:43:24 -0700

    traverse tree - insert function

--- Assignment06/Main.cpp
+++ Assignment06/Main.cpp
@@ -1,4 +1,4 @@
-
+// Prime numbers: 3,5,7,11,13,17,19,23,29,31,37,39,41,43,47
 
 #include <array>
 #include <iostream>
@@ -10,9 +10,30 @@ using namespace std;
 // Node structure
 struct Node {
     int value;
-    Node *right;
     Node *left;
+    Node *right;
 };
+
+Node *newNode(int value) {
+    Node *node = new Node;
+    node->value = value;
+    node->left = NULL;
+    node->right = NULL;
+
+    return node;
+}
+
+Node *insert(Node *root, int value) {
+
+    if(value < root->value) {
+        root->left = insert(root->left, value);
+    } else if (value > root->value) {
+        root->right = insert(root->right, value);
+    }
+
+    return root;
+}
+
 
 
 
