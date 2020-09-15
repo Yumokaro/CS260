84ead612f57fa9183fe78e88fb3620a92f3f5990
Yumokaro <yumokaro18@gmail.com>
2020-09-14 19:25:26 -0700

    add stuff

--- Assignment07/hash_table.h
+++ Assignment07/hash_table.h
@@ -2,6 +2,17 @@
 #ifndef CS260_HASH_TABLE_H
 #define CS260_HASH_TABLE_H
 
+#include <iostream>
+// STLs
+#include <list>
+#include <cstring>
+
+#include "hash_table.h"
+
+//using std::cout;
+//using std::endl;
+using namespace std;
+
 class hash_table {
 
 };
