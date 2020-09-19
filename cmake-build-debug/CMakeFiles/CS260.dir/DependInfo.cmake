497cd96691f55be772700d88794ed1ed6178c1da
Yumokaro <yumokaro18@gmail.com>
2020-09-18 23:40:52 -0700

    update cmake add heading to main

--- cmake-build-debug/CMakeFiles/CS260.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/CS260.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/Tempo/OneDrive/Documents/Code Projects/School/CS260/Assignment07/Main.cpp" "C:/Users/Tempo/OneDrive/Documents/Code Projects/School/CS260/cmake-build-debug/CMakeFiles/CS260.dir/Assignment07/Main.cpp.obj"
+  "C:/Users/Tempo/OneDrive/Documents/Code Projects/School/CS260/Assignment05/Main.cpp" "C:/Users/Tempo/OneDrive/Documents/Code Projects/School/CS260/cmake-build-debug/CMakeFiles/CS260.dir/Assignment05/Main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
